88a3332a510c609a7f9e6e4bff4407bf98d8722b
zxhamo <38841829+zxhamo@users.noreply.github.com>
2018-05-24 04:49:19 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+onst Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+      }
+});
+
+وضع الكودات 
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
