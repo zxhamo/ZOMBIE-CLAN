787eb92b8f62b7accb9e664350a5441f2e952c6f
zxhamo <38841829+zxhamo@users.noreply.github.com>
2018-05-25 09:21:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,23 +5,6 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-      }
-});
-
-client.on('message', message => {
-if (message.content.startsWith('برب')){
-     let embed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("random")
-  .addField(" ****" , " ٌــيَــتٌ يَ حٍــلُــۆ،  لُآ تٌــطًــۆلُ :disappointed:")
-     
-     
-  message.channel.sendEmbed(embed);
-    }
-});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
