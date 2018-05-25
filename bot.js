b115684a4842a5da34fe1d2e0228dd88958ed8ae
zxhamo <38841829+zxhamo@users.noreply.github.com>
2018-05-25 09:32:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,11 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-            if (message.content === 'باك') {
-              message.channel.sendFile("ولكم");
-            }
-         });
+client.on('ready', () => {
+     client.user.setActivity("ولكمباك",{type: 'WATCHING'});
+
+});
+
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
