fba4fb3faac264a3b6009f79f232f3aeba0af6a7
zxhamo <38841829+zxhamo@users.noreply.github.com>
2018-05-25 09:14:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,32 @@ client.on('message', message => {
         message.reply('pong');
       }
 });
+
+client.on('message', message => {
+if (message.content.startsWith('باك')){
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("random")
+  .addField(" **تٌــيَــتٌ يَ حٍــلُــۆ،  لُآ تٌــطًــۆلُ :disappointed:**" , " **تٌــيَــتٌ يَ حٍــلُــۆ،  لُآ تٌــطًــۆلُ :disappointed:**")
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+client.on('message', message => {
+if (message.content.startsWith('برب')){
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("random")
+  .addField(" **تﯡڷــﯖــم ۖ مــڼــﯜڔ ۛ ּڀــ̍ا ̨؏ــڛــڶ ۖ مــن ڙمــٱن ּ؏ــڼــک :green_heart:**" , " **تﯡڷــﯖــم ۖ مــڼــﯜڔ ۛ ּڀــ̍ا ̨؏ــڛــڶ ۖ مــن ڙمــٱن ּ؏ــڼــک :green_heart:**")
+     
+     
+  message.channel.sendEmbed(embed);
+    }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
