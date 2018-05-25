fb93b5962f1a1207cb187a47e00e9ab865d706d2
zxhamo <38841829+zxhamo@users.noreply.github.com>
2018-05-25 09:18:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,21 +16,7 @@ if (message.content.startsWith('باك')){
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("random")
-  .addField(" **تٌــيَــتٌ يَ حٍــلُــۆ،  لُآ تٌــطًــۆلُ :disappointed:**" , " **تٌــيَــتٌ يَ حٍــلُــۆ،  لُآ تٌــطًــۆلُ :disappointed:**")
-     
-     
-  message.channel.sendEmbed(embed);
-    }
-});
-
-
-
-client.on('message', message => {
-if (message.content.startsWith('برب')){
-     let embed = new Discord.RichEmbed()
-  .setAuthor(message.author.username)
-  .setColor("random")
-  .addField(" **تﯡڷــﯖــم ۖ مــڼــﯜڔ ۛ ּڀــ̍ا ̨؏ــڛــڶ ۖ مــن ڙمــٱن ּ؏ــڼــک :green_heart:**" , " **تﯡڷــﯖــم ۖ مــڼــﯜڔ ۛ ּڀــ̍ا ̨؏ــڛــڶ ۖ مــن ڙمــٱن ּ؏ــڼــک :green_heart:**")
+  .addField("" , "تﯡڷــﯖــم ۖ مــڼــﯜڔ ۛ ּڀــ̍ا ̨؏ــڛــڶ ۖ مــن ڙمــٱن ּ؏ــڼــک :green_heart:")
      
      
   message.channel.sendEmbed(embed);
